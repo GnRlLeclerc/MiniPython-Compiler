a1adea6bf7efe15ed0e689c0e4699f01f4a18265
Thibaut de Saivre <thibaut.de-saivre@polytechnique.edu>
2024-01-29 15:57:17 +0100

    format settings

--- src/mini_python/Compile.java
+++ src/mini_python/Compile.java
@@ -2,10 +2,10 @@ package mini_python;
 
 class Compile {
 
-  static boolean debug = false;
+	static boolean debug = false;
 
-  static X86_64 file(TFile f) {
-    return null; // TODO
-  }
+	static X86_64 file(TFile f) {
+		return null; // TODO
+	}
 
 }
--- src/mini_python/Typing.java
+++ src/mini_python/Typing.java
@@ -2,16 +2,16 @@ package mini_python;
 
 class Typing {
 
-  static boolean debug = false;
+	static boolean debug = false;
 
-  // use this method to signal typing errors
-  static void error(Location loc, String msg) {
-    throw new Error(loc + "\nerror: " + msg);
-  }
+	// use this method to signal typing errors
+	static void error(Location loc, String msg) {
+		throw new Error(loc + "\nerror: " + msg);
+	}
 
-  static TFile file(File f) {
-    error(null, "TODO");
-    return null;
-  }
+	static TFile file(File f) {
+		error(null, "TODO");
+		return null;
+	}
 
 }
