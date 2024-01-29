26f4ef361cb141ed5cdbef5778a8e02d9f1f0543
Paul Breuil <tykapl.breuil@gmail.com>
2024-01-29 18:09:13 +0100

    add good architecturee

--- src/mini_python/Typer.java
+++ src/mini_python/Typer.java
@@ -4,119 +4,130 @@ import java.util.HashMap;
 
 // the following exception is used whenever you have to implement something
 class Todo extends Error {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	Todo() {
-		super("TODO");
-	}
+    Todo() {
+        super("TODO");
+    }
 }
 
 /* The typer starts here */
 class Typer implements Visitor {
 
-	// functions definitions (functions are global, hence `static`)
-	static HashMap<String, Def> functions = new HashMap<>();
-	static TFile tFile = new TFile();
-	// local variables
-	public HashMap<String, Variable> vars;
+    // functions definitions (functions are global, hence `static`)
+    static HashMap<String, Def> functions = new HashMap<>();
+    // local variables
+    public HashMap<String, Variable> vars;
+    public TStmt currStmt;
+    private TExpr currExpr;
 
-	Typer() {
-		this.vars = new HashMap<>();
-	}
+    Typer() {
+        this.vars = new HashMap<>();
+        this.currStmt = null;
+        this.currExpr = null;
+    }
 
-	// interpreting constants is immediate
-	public void visit(Cnone c) {
-		throw new Todo();
-	}
+    // interpreting constants is immediate
+    public void visit(Cnone c) {
+        throw new Todo();
+    }
 
-	public void visit(Cbool c) {
-		throw new Todo();
-	}
+    public void visit(Cbool c) {
+        throw new Todo();
+    }
 
-	public void visit(Cstring c) {
-		throw new Todo();
-	}
+    public void visit(Cstring c) {
+        throw new Todo();
+    }
 
-	public void visit(Cint c) {
-		throw new Todo();
-	}
+    public void visit(Cint c) {
+        throw new Todo();
+    }
 
-	// interpreting expressions
+    // interpreting expressions
 
-	@Override
-	public void visit(Ecst e) {
-		throw new Todo();
-	}
+    @Override
+    public void visit(Ecst e) {
+        TEcst tEcst = new TEcst(e.c);
+        this.currExpr = tEcst;
+    }
 
-	@Override
-	public void visit(Ebinop e) {
-		throw new Todo();
-	}
+    @Override
+    public void visit(Ebinop e) {
+        throw new Todo();
+    }
 
-	@Override
-	public void visit(Eunop e) {
-		throw new Todo();
-	}
+    @Override
+    public void visit(Eunop e) {
+        throw new Todo();
+    }
 
-	@Override
-	public void visit(Eident id) {
-		throw new Todo();
-	}
+    @Override
+    public void visit(Eident id) {
+        throw new Todo();
+    }
 
-	@Override
-	public void visit(Ecall e) {
-		throw new Todo();
-	}
+    @Override
+    public void visit(Ecall e) {
+        throw new Todo();
+    }
 
-	@Override
-	public void visit(Elist e) {
-		throw new Todo();
-	}
+    @Override
+    public void visit(Elist e) {
+        throw new Todo();
+    }
 
-	@Override
-	public void visit(Eget e) {
-		throw new Todo();
-	}
+    @Override
+    public void visit(Eget e) {
+        throw new Todo();
+    }
 
-	// interpreting statements
+    // interpreting statements
 
-	@Override
-	public void visit(Seval s) {
-		throw new Todo();
-	}
+    @Override
+    public void visit(Seval s) {
+        throw new Todo();
+    }
 
-	@Override
-	public void visit(Sprint s) {
-		throw new Todo();
-	}
+    @Override
+    public void visit(Sprint s) {
+        s.e.accept(this);
+        TSprint tsprint = new TSprint(this.currExpr);
+        this.currStmt = tsprint;
+    }
 
-	@Override
-	public void visit(Sblock s) {
-		throw new Todo();
-	}
+    @Override
+    public void visit(Sblock s) {
+        TSblock tsblock = new TSblock();
+        for (Stmt stmt : s.l) {
+            stmt.accept(this);
+            tsblock.l.add(this.currStmt);
+        }
+        this.currStmt = tsblock;
+    }
 
-	@Override
-	public void visit(Sif s) {
-		throw new Todo();
-	}
+    @Override
+    public void visit(Sif s) {
+        throw new Todo();
+    }
 
-	@Override
-	public void visit(Sassign s) {
-		throw new Todo();
-	}
+    @Override
+    public void visit(Sassign s) {
+        throw new Todo();
+    }
 
-	@Override
-	public void visit(Sreturn s) {
-		throw new Todo();
-	}
+    @Override
+    public void visit(Sreturn s) {
+        throw new Todo();
+    }
 
-	@Override
-	public void visit(Sfor s) {
-		throw new Todo();
-	}
+    @Override
+    public void visit(Sfor s) {
+        throw new Todo();
+    }
 
-	@Override
-	public void visit(Sset s) {
-		throw new Todo();
-	}
+    @Override
+    public void visit(Sset s) {
+        throw new Todo();
+    }
 }
--- src/mini_python/Typing.java
+++ src/mini_python/Typing.java
@@ -1,6 +1,7 @@
 package mini_python;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 
 class Typing {
 
@@ -12,34 +13,41 @@ class Typing {
 	}
 
 	static TFile file(File f) {
+		TFile tFile = new TFile();
+
 		for (Def d : f.l)
 			Typer.functions.put(d.f.id, d);
 
-		// Visit the main function. We will keep the local variables of main() as global variables
+		// Visit the main function. We will keep the local variables of main() as global
+		// variables
 		Typer mainTyper = new Typer();
 		f.s.accept(mainTyper);
+		LinkedList<Variable> mainParams = new LinkedList<Variable>();
+		Function mainFunc = new Function("main", mainParams);
+		TDef mainTDef = new TDef(mainFunc, mainTyper.currStmt);
+
+		// Add the main function to the function list
+		tFile.l.add(mainTDef);
 
 		// Visit all other functions
-		for (Def d : f.l) {
+		for (Def def : f.l) {
 			Typer typer = new Typer();
 			// Create a copy instead of using the same
 			typer.vars = new HashMap<>(mainTyper.vars);
+			LinkedList<Variable> params = new LinkedList<Variable>();
+			for (Ident i : def.l) {
+				Variable v = Variable.mkVariable(i.id);
+				params.push(v);
+				typer.vars.put(i.id, v);
+			}
 
 			// Accept this function and add it to the function list
-			d.s.accept(typer);
+			def.s.accept(typer);
+			Function func = new Function(def.f.id, params);
+			TDef tdef = new TDef(func, typer.currStmt);
+			tFile.l.add(tdef);
 		}
-
-
-		// for (Def def : f.l) {
-		// 	LinkedList<Variable> params = new LinkedList<Variable>();
-		// 	for (Ident i : def.l) {
-		// 		Variable v = Variable.mkVariable(i.id);
-		// 		params.push(v);
-		// 	}
-		// 	Function func = new Function(def.f.id, params);
-		// 	TDef tdef = new TDef(func, null);
-		// }
-		return Typer.tFile;
+		return tFile;
 	}
 
 }
